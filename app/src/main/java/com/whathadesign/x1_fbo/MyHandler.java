d2fe2c9e78034ee63b618f43d2985a5c3187d79a
Whatha <elwhatha@hotmail.com>
2018-10-10 22:19:18 -0500

    tech

--- app/src/main/java/com/whathadesign/x1_fbo/MyHandler.java
+++ app/src/main/java/com/whathadesign/x1_fbo/MyHandler.java
@@ -1,12 +1,22 @@
 package com.whathadesign.x1_fbo;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,6 +24,8 @@ import java.lang.ref.WeakReference;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  * This handler will be passed to UsbService. Data received from serial port is displayed through this handler
@@ -100,8 +112,9 @@ public class MyHandler extends Handler {
             }else   if(cmdValue.equals("1E") && !systemValue.equals("W&M")) {
                 Float dataValue = Float.parseFloat(json.getString("Data"));
                 int metro = Math.round(dataValue);
+                UpdateMeters(Static_variables.selected,metro,0);
                 mActivity.get().dd.dismiss();
-                Static_variables.selected.getMetros().get(0).currentValue=metro;
+
                 Intent a = new Intent(c, Fuel_feed.class);
                 a.putExtra("status", "Fueling completed!");
                 c.startActivity(a);
@@ -133,6 +146,83 @@ public class MyHandler extends Handler {
         //mActivity.get().display.append("Value Gross Gal: " + valueVolume + "\n");
         counter = 0;
         dataByte = new ArrayList<Byte>();
+    }
+
+    private void UpdateMeters(final Truck t, final int newCurrent1, final int newCurrent2) {
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(c);
+
+
+        JSONArray cosito = new JSONArray();
+        JSONObject MyData = new JSONObject();
+
+        try {
+            MyData.put("ItemId", t.masterItemId);
+            MyData.put("Id", t.getId());
+            MyData.put("NewBalance", t.getBalance());
+            MyData.put("operationDate", Static_variables.todayDate());
+//Add the data you'd like to send to the server.
+            JSONArray Meters = new JSONArray();
+            JSONObject meter1 = new JSONObject();
+            JSONObject meter2 = new JSONObject();
+            try {
+                meter1.put("meterId", t.getMetros().get(0).id);
+                meter1.put("CurrentValue", t.getMetros().get(0).currentValue);
+                meter1.put("NewCurrentValue", newCurrent1);
+
+                Meters.put(meter1);
+
+
+                if (t.getMetros().size() > 1) {
+                    meter2.put("meterId", t.getMetros().get(1).id);
+                    meter2.put("CurrentValue", t.getMetros().get(1).currentValue);
+                    meter2.put("NewCurrentValue", newCurrent2);
+
+                    Meters.put(meter2);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            MyData.put("Meters", Meters); //Add the data you'd like to send to the server.
+            cosito.put(MyData);
+            System.out.println("LEFIN" + cosito);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Fuel_feed.CustomDos req = new Fuel_feed.CustomDos(Request.Method.POST, Static_variables.Apis + "addOpenVerifiedTruck", cosito,
+                new Response.Listener<JSONArray>() {
+
+                    @Override
+
+                    public void onResponse(JSONArray response) {
+                        Truck a = Static_variables.selected;
+                        a.getMetros().get(0).currentValue = newCurrent1;
+                        if (a.getMetros().size() > 1) {
+                            a.getMetros().get(1).currentValue = newCurrent2;
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Toast.makeText(c, "Error!", Toast.LENGTH_SHORT).show();
+
+                        System.out.println("ERROR CARAJO");
+                    }
+                }) {
+
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap();
+                headers.put("Authorization", "bearer " + Static_variables.Token);
+                return headers;
+            }
+        };
+
+
+        // Adding request to request queue
+        MyRequestQueue.add(req);
     }
 }
 
